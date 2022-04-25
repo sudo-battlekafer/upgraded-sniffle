eb7485285507eb5a62099b7d4833b3502c914af8
Eric Tucker <battlekafer@gmail.com>
2022-04-25 11:22:35 -0500

    updating user-data, build.sh

--- build.sh
+++ build.sh
@@ -14,4 +14,4 @@ echo ">Let's get building!"
 packer build -force -on-error=ask -var-file variables.pkrvars.hcl -var-file vsphere.pkrvars.hcl ubuntu-20.04.pkr.hcl
 
 echo ">Deleting port forward for security"
-powershell.exe 'netsh interface portproxy delete v4tov4 listenport=4000 listenaddress=0.0.0.0 connectport=4000 connectaddress=127.0.0.1'
+powershell.exe 'netsh interface portproxy delete v4tov4 listenport=4000 listenaddress=0.0.0.0'
